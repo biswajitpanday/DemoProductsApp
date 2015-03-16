ee625ab05f10823df3d9145748eff2b73551466d
biswajitpanday <biswajit.panday@bs-23.com>
2015-03-16 18:51:28 +0600

    First Commit

---
+++ ProductsApp/Controllers/ProductsController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using ProductsApp.Models;
+
+namespace ProductsApp.Controllers
+{
+    public class ProductsController : ApiController
+    {
+        Product[] products = new Product[]
+        {
+            new Product { Id = 1, Name = "Suchi", Category = "Itelean", Price= 670 },
+            new Product { Id = 2, Name = "Beef chuck steak grill", Category = "London", Price = 550 },
+            new Product { Id = 3, Name = "Fruit Salad", Category = "US", Price = 750 }
+        };
+
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return products;
+        }
+
+        public IHttpActionResult GetProduct(int id)
+        {
+            var product = products.FirstOrDefault(p => p.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return Ok(product);
+        }
+ 
+    }
+}
